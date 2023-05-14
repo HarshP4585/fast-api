0a4bb74c75ba8090b30da56677e5c9640577b4cf
Harsh P <pharsh4585@gmail.com>
2023-05-14 20:49:46 +0530

    basic schema using pydantic model

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# Schema
+# - get all the values from the body
+# - data validation
+# - force the (client/frontend) to send data in a schema that we expect
+# - using "pydantic" -> define schema (can be used with any python code, independent from fastAPI)
+
+class Post(BaseModel): # Post Model
+    # title: string
+    # content: string
+    
+    title: str
+    content: str
+    published: bool = True # optional field, default value
+    rating: Optional[int] = None # optional field, default to None
 
 @app.get("/get_data")
 def get_data():
     return {"key": "value"}
 
 @app.post("/post_data")
-def post_data(payload: dict = Body(...)):
-    return {"created": payload}
+# def post_data(payload: dict = Body(...)):
+def post_data(post_payload: Post):
+    # pydantic model -> dict
+    return {"created": post_payload.dict()}
