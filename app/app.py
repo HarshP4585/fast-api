1f1fac5a2cec0db8ff60e57072906a6580f6c0d9
Harsh P <pharsh4585@gmail.com>
2023-05-23 15:22:55 +0530

    added cors middleware, requirements.txt and example .env file

--- app/app.py
+++ app/app.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import accounts, posts, users, votes
 
 app = FastAPI()
+
+origins = [ # strict access to API
+    "https://www.google.com",
+    "https://www.youtube.com"
+]
+
+app.add_middleware( # a function that runs before every request
+    CORSMiddleware,
+    allow_origins=origins, # ["*"] -> public API
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(accounts.router)
@@ -56,3 +71,13 @@ app.include_router(votes.router)
 #   This will create a revision python file where upgrade and downgrade steps are to be mentioned
 # reflect migration to the db -> alembic upgrade revision_id
 # use --autogenerate flag to sync changes between DB and SQLAlchemy models
+
+# Getting data in the browser from API call:
+# fetch("http://localhost:8000/posts").then(resp => resp.json()).then(console.log)
+# - Promise {<pending>}
+# - Access to fetch at 'http://localhost:8000/posts' from origin 'https://www.google.com' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+# blocked by CORS (Cross Origin Resource Sharing) policy
+# the origin of request is mentioned in the request header
+# cors allow to make request from web browser on one domain to a server on different domain
+# by default our API will only allow web browsers running on the same domain to make request to it
+# add cors middleware to the app
